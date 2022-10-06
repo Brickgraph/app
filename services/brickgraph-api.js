2f4e68abee706d7abc0fedc9a2314e432bd64a32
David Bender <bender2242@gmail.com>
2022-10-06 18:24:42 +0100

    made axios module for brickgraph backend

--- services/brickgraph-api.js
+++ services/brickgraph-api.js
@@ -3,5 +3,5 @@ import Axios from "axios";
 export const brickgraph = Axios.create({
   headers: { "Content-Type": "application/json" },
   authorization: "",
-  baseURL: "",
+  baseURL: process.env.BRICKGRAPH_API_URL,
 });
