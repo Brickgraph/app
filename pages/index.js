a170f5ebb2243b72a396f8f186c138dce062959c
David Bender <bender2242@gmail.com>
2022-10-07 18:21:30 +0100

    connected to backend api, added user details on index

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,43 @@
 import { withServerSideAuth } from "@clerk/nextjs/ssr";
 import { getUserById, getSessionById } from "../utils/users";
+import { brickgraph } from "../services/brickgraph-api";
+import { useState } from "react";
 
 const clerkAPIKEY = process.env.CLERK_API_KEY;
 
 export default function Home({ user, session }) {
+  // Function to fetch data from an API
+  const [data, setData] = useState({ data: [] });
+  const [isLoading, setIsLoading] = useState(false);
+  const handleClick = async () => {
+    setIsLoading(true);
+    const response = await brickgraph.get("/auth/test");
+    console.log(response);
+    setData(response.data);
+    setIsLoading(false);
+  };
+
   return (
-    <div>
-      <span className="text-3xl">Hello {user.first_name}</span>
-    </div>
+    <>
+      <div>
+        <span className="text-3xl">Hello {user.first_name}</span>
+        <br />
+        <button onClick={handleClick} disabled={isLoading}>
+          Test Backend
+        </button>
+      </div>
+      <div>
+        {isLoading && <h2>Loading...</h2>}
+        {!isLoading && (
+          <div key={data.user_id}>
+            <h2>{data.email}</h2>
+            <h2>{data.subscription}</h2>
+            <h2>{data.is_admin}</h2>
+            <br />
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
--- services/brickgraph-api.js
+++ services/brickgraph-api.js
@@ -3,5 +3,5 @@ import Axios from "axios";
 export const brickgraph = Axios.create({
   headers: { "Content-Type": "application/json" },
   authorization: "",
-  baseURL: process.env.BRICKGRAPH_API_URL,
+  baseURL: "http://127.0.0.1:8000",
 });
