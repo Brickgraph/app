ff4bc48600939b56967e05e1741bf6a4b03853b4
David Bender <bender2242@gmail.com>
2022-10-17 01:50:07 +0100

    added auto redraw on window resizing, and refresh button now hovers

--- components/visualisations/graph/visGraph.js
+++ components/visualisations/graph/visGraph.js
@@ -3,25 +3,66 @@ import React, { useState, useEffect } from "react";
 import ModalBase from "../../modals/modalBase";
 import { nodesDictFunc } from "./toDictFunctions";
 
-const GraphVisual = ({ isVisible, options, events, data }) => {
-  if (isVisible !== true) {
+export const GraphVisual = ({
+  options,
+  events,
+  data,
+  height = "100%",
+  width = "100%",
+}) => {
+  const [dataLoading, setIsDataLoading] = useState(true);
+  const resetData = () => {
+    setIsDataLoading((current) => !current);
+  };
+
+  // Refreshes graph when button is clicked
+  useEffect(() => {
+    var waitTime = 1000;
+    setTimeout(() => {}, waitTime);
+    setIsDataLoading(true);
+  }, [dataLoading]);
+
+  // Refreshes graph when window is resized
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDataLoading(false);
+    };
+    window.addEventListener("resize", handleResize);
+    return (_) => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
+  if (dataLoading !== true) {
     return (
       <>
-        <div>Getting the graph for you now...</div>
+        <div>Loading...</div>
       </>
     );
   }
 
   return (
     <>
-      <Graph
-        graph={data}
-        options={options}
-        events={events}
-        style={{ height: "600px", width: "100%" }}
-        clusterThreshold={100}
-        physics={{ enabled: true, solver: "forceAtlas2Based" }}
-      />
+      <div className="relative h-full">
+        <div className="absolute top-0 right-0 transform z-10">
+          <div>
+            <button
+              onClick={resetData}
+              className="text-sm text-bold rounded border-orange-500 border-1 p-2 hover:bg-orange-500 text-grey-800 hover:text-white"
+            >
+              Refresh
+            </button>
+          </div>
+        </div>
+        <Graph
+          graph={data}
+          options={options}
+          events={events}
+          style={{ height: height, width: width }}
+          clusterThreshold={100}
+          physics={{ enabled: true, solver: "forceAtlas2Based" }}
+        />
+      </div>
     </>
   );
 };
@@ -30,91 +71,73 @@ const VisGraph = ({ status, data }) => {
   const [responseStatus, setResponseStatus] = useState(status);
   if (responseStatus !== 200) {
     return <div>No Data available right now... Please try again later.</div>;
-  } else {
-    const nodesDict = nodesDictFunc(data);
-    const [dataLoading, setIsDataLoading] = useState(true);
-    const [nodeHovered, setNodeHovered] = useState(null);
-    const [nodeSelected, setNodeSelected] = useState(null);
-    const [isModalVisible, setIsModalVisible] = useState(false);
+  }
 
-    const resetData = () => {
-      setIsDataLoading((current) => !current);
-    };
+  const nodesDict = nodesDictFunc(data);
+  const [nodeHovered, setNodeHovered] = useState(null);
+  const [nodeSelected, setNodeSelected] = useState(null);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-    useEffect(() => {
-      var waitTime = 1000;
-      setTimeout(() => {}, waitTime);
-      setIsDataLoading(true);
-    }, [dataLoading]);
+  const options = {
+    layout: {
+      hierarchical: false,
+    },
+    nodes: {
+      shape: "circle",
+      size: 12,
+    },
+    edges: {
+      color: "orange",
+      physics: false,
+    },
+    interaction: { hover: true },
+  };
 
-    const options = {
-      layout: {
-        hierarchical: false,
-      },
-      nodes: {
-        shape: "circle",
-        size: 12,
-      },
-      edges: {
-        color: "orange",
-        physics: false,
-      },
-      interaction: { hover: true },
-    };
-
-    const [state, setState] = useState(
-      {
-        counter: 5,
-        graph: data,
-        events: {
-          selectNode: function (e) {
-            var { nodes, edges } = e;
-            var nodeId = nodes;
-            var node = nodesDict[nodeId];
-            setNodeSelected(node);
-            setIsModalVisible(true);
-          },
-          hoverNode: ({ node }) => {
-            setNodeHovered(node);
-          },
+  const [state, setState] = useState(
+    {
+      counter: 5,
+      graph: data,
+      events: {
+        selectNode: function (e) {
+          var { nodes, edges } = e;
+          var nodeId = nodes;
+          var node = nodesDict[nodeId];
+          setNodeSelected(node);
+          setIsModalVisible(true);
+        },
+        hoverNode: ({ node }) => {
+          setNodeHovered(node);
         },
       },
-      []
-    );
-    const { graph, events } = state;
+    },
+    []
+  );
 
-    return (
-      <>
-        <button
-          onClick={resetData}
-          className="bg-orange-400 rounded border-black border-1 p-2 hover:bg-orange-500 text-grey-800 hover:text-white"
-        >
-          Redraw Graph
-        </button>
-        <div>
-          <span className="inset-10 z-10 text-bold text-md lg:text-lg">
-            {nodeHovered}
-          </span>
-        </div>
+  const { graph, events } = state;
+
+  return (
+    <div className="flex flex-col">
+      <div>
         <GraphVisual
-          isVisible={dataLoading}
           options={options}
           events={events}
           data={graph}
+          height={"500px"}
+          width={"100%"}
         />
+      </div>
 
-        <ModalBase
-          isVisible={isModalVisible}
-          onClose={() => setIsModalVisible(false)}
-        >
-          <h1 className="text-2xl text-black">
-            {nodeSelected ? nodeSelected.id : ""} -{" "}
-            {nodeSelected ? nodeSelected.group : ""}
-          </h1>
-        </ModalBase>
-      </>
-    );
-  }
+      <ModalBase
+        isVisible={isModalVisible}
+        onClose={() => setIsModalVisible(false)}
+      >
+        <h1 className="text-2xl text-black">
+          {nodeSelected ? nodeSelected.id : ""} -{" "}
+          {nodeSelected ? nodeSelected.group : ""}
+        </h1>
+      </ModalBase>
+    </div>
+  );
 };
 
 export default VisGraph;
--- pages/index.js
+++ pages/index.js
@@ -3,36 +3,13 @@ import { getUserById, getSessionById } from "../utils/users";
 import { brickgraph } from "../services/brickgraph-api";
 import { useState } from "react";
 import VisGraph from "../components/visualisations/graph/visGraph";
-import { visData } from "../components/visualisations/graph/testData_vis";
 import { setAuthorizationHeader } from "../services/auth";
 
 export default function Home({ user, backendStatus, backendData }) {
-  const [data, setData] = useState({ data: [] });
-  const [isLoading, setIsLoading] = useState(false);
-  const handleClick = async () => {
-    setIsLoading(true);
-    const response = await testBackend();
-    if (response.status === 200) {
-      setData(response.data);
-      console.log("SUCCESS", response.data);
-      setIsLoading(false);
-    }
-    console.log(response.status);
-  };
-
   return (
     <>
       <div>
-        <span className="text-md md: text-lg xl:text-3xl">
-          Hello {user.first_name}
-        </span>
-        <br />
-        <button onClick={handleClick}>Test</button>
-        <br />
-        {/* <div id="vis" className="p-4 w-[90%] pl-4 border-2 border-rose-600">
-          <VisGraph graphData={graphData} />
-        </div> */}
-        <div className="p-4 w-[90%] pl-4 border-2 border-orange-500">
+        <div className="p-4 h-[100%] w-[100%] flex items-center">
           <VisGraph status={backendStatus} data={backendData.graph} />
         </div>
       </div>
@@ -52,7 +29,6 @@ export const getServerSideProps = withServerSideAuth(
 
     const user = await getUserById(userId);
     const token = await getToken();
-    //const graphData = visData;
 
     // Backend data to populate graph
     setAuthorizationHeader(token);
