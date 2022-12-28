13138e6256abee7cb245759e3a464ecb1d004a00
David Bender <bender2242@gmail.com>
2022-12-28 20:28:43 +0000

    updated button

--- components/modals/relationshipDetails.js
+++ components/modals/relationshipDetails.js
@@ -5,6 +5,7 @@ export const RelationshipDetailsModal = ({ relationshipId, onClose, show }) => {
   return (
     <ModalBase onClose={onClose} show={show}>
       <div>Relationship Details {relationshipId}</div>
+      <button onClick={onClose}>Close</button>
     </ModalBase>
   );
 };
