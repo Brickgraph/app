ee8842663b1f1785321b76b3ad9b34f9a2889a6f
David Bender <bender2242@gmail.com>
2023-01-08 18:55:30 +0000

    debugged startup error

--- next.config.js
+++ next.config.js
@@ -4,11 +4,5 @@ const nextConfig = {
   swcMinify: true,
 };
 
-const withTM = require("next-transpile-modules")([
-  "react-cytoscapejs",
-  "react-force-graph-2d",
-]);
-
 module.exports = nextConfig;
-module.exports = withTM({ ...nextConfig });
 module.exports = { images: { domains: ["images.clerk.dev"] } };
